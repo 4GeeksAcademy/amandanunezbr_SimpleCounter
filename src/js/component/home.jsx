2c9262ad2bb0761e608458f86977c9ecc0eb136f
amandanunezbr <gerencialmonsters@gmail.com>
2024-11-27 19:37:31 +0000

    Proyecto finalizado.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,23 @@
-import React from "react";
+import React from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function SecondCounter(props) {  
+  let seconds = 0;
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  setInterval(() => {
+    seconds += 1;
+    
+    const counterElement = document.getElementById("seconds-counter");
+    if (counterElement) {
+      counterElement.innerText = String(seconds).padStart(6, "0");
+    }
+  }, 1000);
 
-export default Home;
+  return (
+    <div className='App'>
+      <i class="fa-solid fa-timer"></i>
+      <h2 id="seconds-counter">{seconds}</h2>
+    </div>
+  );
+}
+
+export default SecondCounter;
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,55 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// include your styles into the webpack bundle
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "../styles/index.css";
+import PropTypes from "prop-types";
 
-//import your own components
-import Home from "./component/home.jsx";
+function SimpleCounter(props) {
+    return (
+        <div className="bigCounter">
+            <div className="calendar">
+                <i className="fa-regular fa-clock"></i>
+            </div>
+            <div className="six">{props.digitSix % 10}</div>
+            <div className="five">{props.digitFive % 10}</div>
+            <div className="four">{props.digitFour % 10}</div>
+            <div className="three">{props.digitThree % 10}</div>
+            <div className="two">{props.digitTwo % 10}</div>
+            <div className="one">{props.digitOne % 10}</div>
+        </div>
+    );
+}
 
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+SimpleCounter.propTypes = {
+    digitSix: PropTypes.number,
+    digitFive: PropTypes.number,
+    digitFour: PropTypes.number,
+    digitThree: PropTypes.number,
+    digitTwo: PropTypes.number,
+    digitOne: PropTypes.number
+};
 
+let counter = 0;
+setInterval(function () {
+    const six = Math.floor(counter / 100000);
+    const five = Math.floor(counter / 10000) % 10;
+    const four = Math.floor(counter / 1000) % 10;
+    const three = Math.floor(counter / 100) % 10;
+    const two = Math.floor(counter / 10) % 10;
+    const one = Math.floor(counter / 1) % 10;
+
+    // Render the component with updated values
+    ReactDOM.createRoot(document.getElementById('app')).render(
+        <SimpleCounter
+            digitOne={one}
+            digitTwo={two}
+            digitThree={three}
+            digitFour={four}
+            digitFive={five}
+            digitSix={six}
+        />
+    );
+
+    counter++;
+}, 1000);
